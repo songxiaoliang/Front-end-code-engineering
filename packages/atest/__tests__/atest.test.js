ee111f3585c29fc26df649df834c9f08cb032a99
Songlcy <songxlcy@gmail.com>
2021-05-18 20:35:29 +0800

    delete package

--- packages/atest/__tests__/atest.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const atest = require('..');
-
-describe('atest', () => {
-    it('needs tests');
-});
--- packages/atest/lib/atest.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = atest;
-
-function atest() {
-    // TODO
-}
